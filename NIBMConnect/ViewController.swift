23b6ad6687b4b338b2abc15f67625e019f5a6485
Thilakshi Bandara <>
2019-05-17 07:32:05 +0530

    first commit created SignUp UI created SignIn UI created Forgot Password UI

---
+++ NIBMConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBMConnect
+//
+//  Created by Oshan Madushanka on 5/16/19.
+//  Copyright © 2019 Thilakshi Bandara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
